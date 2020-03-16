9283377189b6322ed55e3e3f9a980dc668fd12c1
Ben Winding <ben.winding@gmail.com>
2020-03-17 00:06:31 +1030

    Fix tests and manifests

--- src/js/background.js
+++ src/js/background.js
@@ -1,36 +1,34 @@
-function cpy(o) {
-  return JSON.parse(JSON.stringify(o));
-}
 // A generic onclick callback function.
 function genericOnClick(info, tab) {
   chrome.tabs.sendMessage(tab.id, "getClickedEl", function(svgString) {
+    console.log('found svg string!', {svgString})
     copyTextToClipboard(svgString);
-    // console.log({svgString})
   });
 }
 
 // Create one test item for each context type.
-var contexts = ["page", "selection", "link", "image"];
-
+const contexts = ["page", "selection", "link", "image"];
+chrome.contextMenus.removeAll()
 for (let context of contexts) {
-  var title = "Test '" + context + "' menu item";
-  var id = chrome.contextMenus.create({
+  const title = `Copy SVG (${context})`;
+  const id = title;
+  chrome.contextMenus.create({
+    id: id,
     title: title,
     contexts: [context],
     onclick: genericOnClick
   });
-  console.log("'" + context + "' item:" + id);
 }
 
 function copyTextToClipboard(text) {
-  //Create a textbox field where we can insert text to. 
-  var copyFrom = document.createElement("textarea");
+  //Create a textbox field where we can insert text to.
+  const copyFrom = document.createElement("textarea");
 
   //Set the text content to be the text you wished to copy.
   copyFrom.textContent = text;
 
-  //Append the textbox field into the body as a child. 
-  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //Append the textbox field into the body as a child.
+  //"execCommand()" only works when there exists selected text, and the text is inside
   //document.body (meaning the text is part of a valid rendered HTML element).
   document.body.appendChild(copyFrom);
 
@@ -38,12 +36,12 @@ function copyTextToClipboard(text) {
   copyFrom.select();
 
   //Execute command
-  document.execCommand('copy');
+  document.execCommand("copy");
 
-  //(Optional) De-select the text using blur(). 
+  //(Optional) De-select the text using blur().
   copyFrom.blur();
 
-  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //Remove the textbox field from the document.body, so no other JavaScript nor
   //other elements can get access to this.
   document.body.removeChild(copyFrom);
 }
